db9c7e6ea8f4e6aa8092c4c4500927ab283b6549
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2024-11-11 14:38:17 -0500

    Update evaluator.py

--- fense/evaluator.py
+++ fense/evaluator.py
@@ -17,16 +17,23 @@ PRETRAIN_ECHECKERS = {
     "none": (None, None)
 }
 
+def _use_new_echecker_loading() -> bool:
+    version = transformers.__version__
+    major, minor, _patch = map(int, version.split("."))
+    return major > 4 or (major == 4 and minor >= 31)
+
 def load_pretrain_echecker(echecker_model, device='cuda', use_proxy=False, proxies=None):
     from .download_utils import RemoteFileMetadata, check_download_resource
     trf_logging.set_verbosity_error()  # suppress loading warnings
     url, checksum = PRETRAIN_ECHECKERS[echecker_model]
-    remote = RemoteFileMetadata(
-        filename=f'{echecker_model}.ckpt',
-        url=url,
-        checksum=checksum)
+    remote = RemoteFileMetadata(filename=f'{echecker_model}.ckpt', url=url, checksum=checksum)
     file_path = check_download_resource(remote, use_proxy, proxies)
     model_states = torch.load(file_path)
+
+    state_dict = model_states['state_dict']
+    if _use_new_echecker_loading():
+        state_dict.pop("encoder.embeddings.position_ids")
+
     clf = BERTFlatClassifier(model_type=model_states['model_type'], num_classes=model_states['num_classes'])
     clf.load_state_dict(model_states['state_dict'])
     clf.eval()
@@ -66,7 +73,8 @@ class Evaluator:
                 batch[k] = v.to(self.device)
             with torch.no_grad():
                 logits = self.echecker(**batch)
-                probs = torch.sigmoid(logits).detach().cpu().numpy()
+                # probs = torch.sigmoid(logits).detach().cpu().numpy()
+                probs = logits.sigmoid().transpose(0, 1).cpu().numpy()
         else:
             probs = []
             for i in trange(0, len(sents), batch_size):
